ee550375d296782568330a8036dc89f499bfba7b
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-08-17 12:33:33 -0500

    got fixtures working

--- culinary_explorer_api/models/dish.py
+++ culinary_explorer_api/models/dish.py
@@ -4,4 +4,5 @@ class Dish(models.Model):
     dish_name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     notes = models.CharField(max_length=100)
+    food_image_url = models.URLField(default='https://example.com/default-image.jpg')
     price = models.CharField(max_length=100)
--- culinary_explorer_api/models/food_log_categories.py
+++ culinary_explorer_api/models/food_log_categories.py
@@ -1,9 +1,9 @@
 from django.db import models
-from .food_log import food_log
-from .categories import categories
+from .food_log import Food_Log
+from .categories import Categories
 
 
-class Categories(models.Model):
+class FoodLogCategories(models.Model):
 
-    food_id = models.ForeignKey(food_log, on_delete=models.CASCADE)
-    category_id = models.ForeignKey(categories, on_delete=models.CASCADE)
+    food_id = models.ForeignKey(Food_Log, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
