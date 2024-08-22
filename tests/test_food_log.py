c2a36a9462d46acbe42658d2534277ff022a63b7
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-08-22 14:17:01 -0500

    got test for delete food log entry working

--- tests/test_food_log.py
+++ tests/test_food_log.py
@@ -53,3 +53,11 @@ class TestFoodLog(APITestCase):
         print(response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
+    def test_delete_food_log(self):
+        """Test to delete a food log entry"""
+        url = reverse('food_log-detail', kwargs={'pk': self.food_log.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
