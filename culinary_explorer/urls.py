61d1e14e1e7ec6935c635a036b654a1ad1bf2b4f
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-08-29 13:37:12 -0500

    wrote tests for restaurants

--- culinary_explorer/urls.py
+++ culinary_explorer/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from culinary_explorer_api.views import register_user, check_user, CategoriesView, FoodLogView, DishView, RestaurantView
+from culinary_explorer_api.views import register_user, check_user, CategoriesView, FoodLogView, DishView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'categories', CategoriesView, 'categories')
 router.register(r'food_log', FoodLogView, 'food_log')
 router.register(r'dish', DishView, 'dish')
-router.register(r'restaurants', RestaurantView, 'restaurant')
+# router.register(r'restaurants', RestaurantView, 'restaurant')
 
 
 urlpatterns = [
