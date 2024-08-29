c77b20e7d1aa140ef748d81a373f2fbbba2af6b2
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-08-29 13:39:30 -0500

    wrote tests for restaurants

--- tests/test_restaurants.py
+++ tests/test_restaurants.py
@@ -13,5 +13,42 @@ class TestRestaurants(APITestCase):
         self.list_url = reverse('restaurants-list')
         
     def test_single_restaurant(self):
+        """Get single restaurants"""
         response = self.client.get(self.detail_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)    
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+    
+    def test_list_all_restaurants(self):
+        """Get all restaurants"""
+        response = self.client.get(self.list_url)
+        all_restaurants = Restaurants.objects.all()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_create_restaurant(self):
+        """Create new restaurant"""
+        url =reverse('restaurants-list')
+        restaurants_payload = {
+            'restaurant_name' : 'Taco Mammmmmmacita',
+            'restaurant_address' : '1234 6th Ave N, Nashville, TN 37208',
+            'website_url' : 'http://www.tacomamacitagermantown.com/',
+        }
+        response = self.client.post(url, restaurants_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)    
+        
+    def test_update_restaurant(self):
+        """Test changing a restaurant"""
+        update_restaurants = {
+            'restaurant_name' : 'Taco Mamacita',
+            'restaurant_address' : '1234 6th Ave N, Nashville, TN 37208',
+            'website_url' : 'http://www.tacomamacitagermantown.com/',            
+        }
+        response = self.client.put(self.detail_url, update_restaurants, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_delete_restaurant(self):
+        """Test to delete a restaurant"""
+        url = reverse('restaurants-detail', kwargs={'pk': self.restaurants.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+                       
