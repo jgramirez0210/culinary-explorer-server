96763cbb66143b0231c55b5b38e7c42504bc64d3
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2025-02-26 12:52:34 -0600

    added uid tracking to restaurant

--- culinary_explorer_api/models/restaurants.py
+++ culinary_explorer_api/models/restaurants.py
@@ -4,3 +4,4 @@ class Restaurants(models.Model):
     restaurant_name = models.CharField(max_length=100)
     restaurant_address = models.CharField(max_length=100)
     website_url = models.CharField(max_length=100)
+    uid = models.CharField(max_length=50, default='aPkqPWh2qYXzL2OHlFunih1ZR3U2')
